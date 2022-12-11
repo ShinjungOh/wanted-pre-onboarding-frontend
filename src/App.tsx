1a11b46e0ea8f5a7ddcf1401571770580e423eb2
ShinjungOh <tlswjd9260@gmail.com>
2022-12-12 06:37:17 +0900

    feat: Layout 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { Routes, Route } from 'react-router';
 
+import { Page } from './ui/components/layout';
 import GlobalStyles from './ui/core/GlobalStyles';
 import Home from './ui/pages/Home';
 import LogIn from './ui/pages/LogIn';
@@ -8,11 +10,13 @@ import SignUp from './ui/pages/SignUp';
 
 const App = () => (
   <>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<LogIn />} />
-      <Route path="/signup" element={<SignUp />} />
-    </Routes>
+    <Page>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Page>
     <GlobalStyles />
   </>
 );
